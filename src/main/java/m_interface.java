3add20f2c1d02f6e3179feadf8a5adadb800bbcf
Omri Hopson <omri@Omris-MacBook-Pro.local>
2019-11-22 11:48:25 -0500

    initial commit

--- src/main/java/m_interface.java
+++ src/main/java/m_interface.java
@@ -164,7 +164,6 @@ public class m_interface extends javax.swing.JFrame {
             public void run() {
                 new m_interface().setVisible(true);
                 String currentDatabase = "";
-                String currentDatabases = "";
             }
         });
     }
